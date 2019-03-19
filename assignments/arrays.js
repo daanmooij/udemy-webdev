3e08f13496735f134ef6a479d6d910990b264a5d
Daan Mooij <daanmooij@gmail.com>
2019-03-19 22:59:34 +0100

    remove console logs and refactor printReverse

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -1,40 +1,41 @@
+// Write a function printReverse() that takes 
+// an array as an argument and prints out 
+// the elements in the array in reverse order
+// (don't actually reverse the array itself)
+
 function printReverse(list) {
-    list.reverse();
-    list.forEach(function(item) {
-        console.log(item);
-    });
+    for (let i = list.length - 1; i >= 0; i--) {
+        console.log(list[i]);
+    }
 }
 
-printReverse([1, 2, 3, 4, 5, 6, 7]);
+// Write a function isUniform() which takes
+// an array as an argument and returns true
+// if all elements in the array are identical
 
 function isUniform(list) {
     var set1 = new Set(list);
     return set1.size > 1 ? false : true; 
 }
 
-console.log(isUniform([1, 1, 1, 1]));
-console.log(isUniform([2, 1, 1, 1]));
+// Write a function sumArray() that accepts
+// an array of numbers and returns the sum
+// of all numbers in the array
 
 function sumArray(list) {
     var sum = list.reduce(function(total, current) {
         return total + current;
     });
-
     return sum;
 }
 
-console.log(sumArray([1, 2, 3]));
-console.log(sumArray([10, 3, 10, 4]));
-console.log(sumArray([-5, 100]));
+// Write a function max() that accepts 
+// an array of numbers and returns 
+// the maximum number in the array
 
 function max(list) {
     var max = list.reduce(function(callback, current) {
         return Math.max(callback, current);
     });
-
     return max;
 }
-
-console.log(max([1, 2, 3]));
-console.log(max([10, 3, 10, 4]));
-console.log(max([-5, 100]));
